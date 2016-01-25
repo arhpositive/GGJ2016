5b4f9d55ee9e3a6015390d3d373382c41abb072c
arhpositive <arhpositive@gmail.com>
2016-01-26 01:09:20 +0200

    folder structure commit

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿/* 
+ * Game: GGJ2016
+ * Author: Arhan Bakan
+ * 
+ * MusicManager.cs
+ * Handles music in battle scene.
+ */
+
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MusicManager : MonoBehaviour
+    {
+        public AudioClip[] MusicList;
+        AudioSource _currentAudioSource;
+
+        void Awake()
+        {
+            if (MusicList.Length == 0)
+            {
+                return;
+            }
+
+            int randomIndex = Random.Range(0, MusicList.Length);
+
+            _currentAudioSource = gameObject.GetComponent<AudioSource>();
+
+            _currentAudioSource.clip = MusicList[randomIndex];
+            _currentAudioSource.loop = true;
+            _currentAudioSource.Play();
+        }
+    }
+}
